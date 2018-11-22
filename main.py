e776ad53b6c17c09aba5e109b4a0879c15c2ec3b
mahi95 <amahaanand@gmail.com>
2018-11-22 13:40:41 +0000

    covered the topics variables up to string concatenation

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print('Hello World')
+a = 1
+print(type(a))
+
+# Always use the same conventions in a file using a string
---
+++ variables.py
@@ -0,0 +1,32 @@
+# Define a variable named city and set it equal to any string
+city = "Chennai"
+
+# Define a variable named price and set it equal to any float
+price = 4.25
+
+# Define a variable named high_score and set it equal to any int
+high_score = 90
+
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
+
+# Set the message variable equal to any string containing a
+# new-line escape sequence
+message = "Hello \n goodbye"
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+
+# Set the quotation variable to any string that contains an escaped
+# double quotation mark
+quotation = "My cat said \"meow\""
+
+# String Concatenation
+greeting = "Hello"
+name = "Mahaanand"
+greet_name = greeting + ' ' + name
+
+
+
+
